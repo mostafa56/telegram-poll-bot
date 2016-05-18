bf07a384f850f40a1e16d947a236de797a675ecf
mostafa56 <mostafa.devloper56@gmail.com>
2016-05-18 16:59:46 +0430

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '210139986:AAG0IqjezPGPGSkE5Xs0VP_i7bJFbTAENRc');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
